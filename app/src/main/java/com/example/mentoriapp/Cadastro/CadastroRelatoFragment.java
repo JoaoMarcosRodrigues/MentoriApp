595b6a746273fa51d8abae3c586664c408b9c62e
João Marcos <jmorodrigues@dcomp.ufs.br>
2021-03-14 16:46:44 -0300

    Finalizando cadastro de relato e criando tela de perfil do mentor e de visita do mentorado.

--- app/src/main/java/com/example/mentoriapp/Cadastro/CadastroRelatoFragment.java
+++ app/src/main/java/com/example/mentoriapp/Cadastro/CadastroRelatoFragment.java
@@ -9,10 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.mentoriapp.R;
 
-public class CadastroRelatoFragment extends Fragment {
+public class CadastroRelatoFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     public static CadastroRelatoFragment getInstance(){
         CadastroRelatoFragment cadastroRelatoFragment = new CadastroRelatoFragment();
@@ -27,7 +32,38 @@ public class CadastroRelatoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro_relato, container, false);
+        View view = inflater.inflate(R.layout.fragment_cadastro_relato, container, false);
+
+        Spinner spinner = view.findViewById(R.id.spinner_tarefas_associadas);
+        Button botao_pronto_relato = view.findViewById(R.id.btn_relato_pronto);
+
+        botao_pronto_relato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment,new CadastroDificuldadeFragment())
+                        .commit();
+            }
+        });
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.numeros, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ app/src/main/java/com/example/mentoriapp/Fragmentos_side/PerfilMentorFragment.java
@@ -0,0 +1,24 @@
+package com.example.mentoriapp.Fragmentos_side;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mentoriapp.R;
+
+public class PerfilMentorFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mentor_perfil, container, false);
+
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return view;
+    }
+}
